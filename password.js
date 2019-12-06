ba79689241990b88d4c7ce19f6b5bdd7ad6b5ced
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-06 08:46:30 -0500

    Create password.js

---
+++ password.js
@@ -0,0 +1,9 @@
+<script>
+function myFunction() {
+  var password = prompt("Please enter your password", "<password goes here>");
+  if (person != null) {
+    document.getElementById("password").innerHTML =
+    "Your pasword is" + person + " Welcome!";
+  }
+}
+</script>
