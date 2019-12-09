ea0436d2ba85acea25670a703c7818acea51698f
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-08 22:28:07 -0500

    Update password.js

--- password.js
+++ password.js
@@ -1,14 +1,21 @@
 <script type="text/javascript">
 
-  function checkPassword(str)
+  function checkPassword(str) //this is the function that will be called later on to check the submitted password
   {
-    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
+  //we need to create a variable that holds the checks for the code
+  // this is is like a look-ahead which basically compares the returned with the future part of the string
+    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/; 
+  // the /d is shrt for [0-9] taht helps identofy if there is at leats one number.
+  //the [a-z] checks for at least one lowercase letter
+  // the [A-Z] checks for at least one uppercase letter
+  // the .{10,}$/ checks for there to be 10 or more characters input
     return re.test(str);
   }
 
-  function checkForm(form)
+  function checkForm(form) // this is the function that will check the form and where the above function will be called
   {
-      if(!checkPassword(form.pwd1.value)) {
+    //the entire if statement is comparing the input from the form to the function af it is not true to the reuqirements in the function, then the message will be displayed
+      if(!checkPassword(form.pwd1.value)) { 
         alert("The password you have entered is not valid!");
         form.pwd1.focus();
         return false;
