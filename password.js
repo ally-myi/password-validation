60492616deac738f2ed80cadb694133067b8fc30
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-08 22:06:38 -0500

    Update password.js

--- password.js
+++ password.js
@@ -1,9 +1,24 @@
-<script>
-function myFunction() {
-  var password = prompt("Please enter your password", "<password goes here>");
-  if (person != null) {
-    document.getElementById("password").innerHTML =
-    "Your pasword is" + person + " Welcome!";
+<script type="text/javascript">
+
+  function checkPassword(str)
+  {
+    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
+    return re.test(str);
   }
-}
+
+  function checkForm(form)
+  {
+      if(!checkPassword(form.pwd1.value)) {
+        alert("The password you have entered is not valid!");
+        form.pwd1.focus();
+        return false;
+      }
+     else {
+      alert("The password you entered meets the requirements");
+      form.pwd1.focus();
+      return true;
+    }
+    return true;
+  }
+
 </script>
